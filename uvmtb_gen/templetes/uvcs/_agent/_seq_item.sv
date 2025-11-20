680bdc773c7b2dd2c49def2d6c299f4c1e41436d
Samic <2925138734@qq.com>
2025-11-20 23:41:32 +0800

    [uvmgen template] update _seq_item.sv manually

--- uvmtb_gen/templetes/uvcs/_agent/_seq_item.sv
+++ uvmtb_gen/templetes/uvcs/_agent/_seq_item.sv
@@ -1,52 +1,33 @@
-{# UVM Agent Jinja2 Template #}
-{# Generate complete UVM Agent components based on YAML configuration #}
+`ifndef {{ agent_name.upper() }}_SEQ_ITEM_SV
+`define {{ agent_name.upper() }}_SEQ_ITEM_SV
 
-{#
-  Template Variables:
-  - agent_name: Agent name (e.g., "axi_master_agent")
-  - config: Configuration dictionary containing valid_enable, work_mode, packages, driver_signals
-#}
-
-{## 1. Generate package import statements ##}
-{% macro generate_package_imports(packages) -%}
-{% for package in packages %}
-import {{ package }}::*;
-{% endfor %}
-{%- endmacro %}
-
-{## 2. Generate signal declarations ##}
-{% macro generate_signal_declarations(signals) -%}
-{% for signal in signals %}
-    {{ signal.type }} {{ signal.signal }};
-{% endfor %}
-{%- endmacro %}
-
-{## 3. Generate sequence_item ##}
-{% set item_filename = agent_name + "_item.sv" %}
-// File: {{ agent_name }}/{{ item_filename }}
-`ifndef {{ agent_name.upper() }}_ITEM_SV
-`define {{ agent_name.upper() }}_ITEM_SV
-
-class {{ agent_name }}_item extends uvm_sequence_item;
+class {{ agent_name }}_seq_item extends uvm_sequence_item;
     
     // Transaction fields based on driver signals
-    {% for signal in config.driver_signals if "clk" not in signal.signal and "rst" not in signal.signal %}
+    {% for signal in config.driver_signals %}
     rand {{ signal.type }} {{ signal.signal }};
     {% endfor %}
     
     // Constraints
-    constraint valid_constraints {
+    constraint signal_constraints {
         // Add transaction constraints here
     }
     
-    `uvm_object_utils_begin({{ agent_name }}_item)
-        {% for signal in config.driver_signals if "clk" not in signal.signal and "rst" not in signal.signal %}
-        `uvm_field_{{ "int" if "[" in signal.type else "enum" }}({{ signal.signal }}, UVM_ALL_ON)
+    `uvm_object_utils_begin({{ agent_name }}_seq_item)
+        {% for signal in config.driver_signals %}
+        `uvm_field_int({{ signal.signal }}, UVM_ALL_ON) // TODO
         {% endfor %}
     `uvm_object_utils_end
     
-    function new(string name = "{{ agent_name }}_item");
+    function new(string name = "{{ agent_name }}_seq_item");
         super.new(name);
+    endfunction
+
+    virtual function void do_print(uvm_printer printer);
+        super.do_print(printer);
+        
+        //printer.print_string("addr", $sformatf("0x%0h", addr));
+
     endfunction
     
 endclass
